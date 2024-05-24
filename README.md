e137fca5d70fe97db835365934f26d29d4319a1f
Andres <job.carlosandres@gmail.com>
2024-05-24 09:58:26 -0500

    Nuevos cambios

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eng-seminary
 Engineering Seminary
+
+
+API : Application Programming Interface
+* UI User Interface
+* CLI Command Line Interface
--- exam_ui.py
+++ exam_ui.py
@@ -4,7 +4,6 @@ import customtkinter as ctk
 # Create the main window
 root = ctk.CTk()
 
-
 # Set the title and size of the window
 root.title("Fancy Application")
 root.geometry("500x300")
